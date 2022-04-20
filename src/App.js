b6e85ae579d9ee21a4aae7045ed54a639187b206
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-20 20:10:42 +0600

    load data from server site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
   return (
     <div className="App">
-      <h1>My users: </h1>
+      <h1>My users: {users.length}</h1>
     </div>
   );
 }
